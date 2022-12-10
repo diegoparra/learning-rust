b3a43e1d75155b96457d4c790b0352f858e71ac4
Diego Parra <dp@linkfire.com>
2022-12-10 17:50:48 -0400

    test

--- reading-input/target/debug/deps/reading_input-432265501b158988.d
+++
@@ -1,5 +0,0 @@
-/Users/di/Documents/wkt/Personal/learning-rust/reading-input/target/debug/deps/reading_input-432265501b158988.rmeta: src/main.rs
-
-/Users/di/Documents/wkt/Personal/learning-rust/reading-input/target/debug/deps/reading_input-432265501b158988.d: src/main.rs
-
-src/main.rs:
--- reading-input/target/debug/deps/reading_input-7df8e4e33891339a.d
+++
@@ -1,5 +0,0 @@
-/Users/di/Documents/wkt/Personal/learning-rust/reading-input/target/debug/deps/reading_input-7df8e4e33891339a: src/main.rs
-
-/Users/di/Documents/wkt/Personal/learning-rust/reading-input/target/debug/deps/reading_input-7df8e4e33891339a.d: src/main.rs
-
-src/main.rs:
